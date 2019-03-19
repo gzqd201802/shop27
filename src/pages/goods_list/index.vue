8c21c7d31a92f126035a26fd2d4c6157608e37d2
Su <gzqd201802@163.com>
2019-03-19 12:06:40 +0800

    下拉加载更多效果

--- src/pages/goods_list/index.vue
+++ src/pages/goods_list/index.vue
@@ -15,28 +15,84 @@
             </view>
         </block>
     </view>
+    <!-- 3.0 商品列表 -->
+    <view class="goods-list">
+        <block
+          v-for="(item,index) in goodsList"
+          :key="index">
+            <view class="goods-item">
+                <image :src="item.goods_small_logo"> </image>
+                <view class="goods-right">
+                    <view class="goods-title">
+                        {{ item.goods_name }}
+                    </view>
+                    <view class="price">
+                        ￥{{ item.goods_price }}
+                    </view>
+                </view>
+            </view>
+        </block>
+    </view>
+    <!-- 4.0 没有数据的提示 -->
+    <view class="loading" v-show="!hasMore">我是有底线的....</view>
   </view>
 </template>
 
 <script>
 // 2.0 导入请求搜索的函数
-import { getSearch } from "@/api";
+import { getSearch } from "@/api/index.js";
 export default {
   data () {
     return{
       keyword:'',
       tabsItems: ["综合","销量","价格"],
-      tabIndex:0
+      tabIndex:0,
+      goodsList: [],
+      pagenum:1,
+      pagesize:20,
+      hasMore: true
     }
   },
   // 1.0 获取页面启动参数只能在 onLoad 生命周期函数中获取
   onLoad(query){
     // 1.0.1 获取页面启动参数中的关键字
     this.keyword = query.keyword;
-    // 2.0 调用函数，发起搜索请求
-    getSearch({
-        query: this.keyword
-    })
+    // 页面加载的时候调用获取数据的函数
+    this.getData();
+  },
+  methods:{
+    // 3.0 封装获取数据的方法
+    getData(){
+        // 4.0 如果 hasMore 假，不要发起请求了
+        if(!this.hasMore) return;
+        // 2.0 调用函数，发起搜索请求
+        getSearch({
+            query: this.keyword,
+            pagenum: this.pagenum,
+            pagesize: this.pagesize
+        }).then(res=>{
+            // 3.0.1 在回调函数中赋值
+            let { goods } = res.data.message;
+            // 3.0.2 解构赋值连接数组
+            this.goodsList = [...this.goodsList , ...goods];
+            // 3.0.2 concat 方法连接数组
+            // this.goodsList = this.goodsList.concat(goods);
+
+            // 3.0.3 请求成功后，页码加 1
+            this.pagenum++;
+
+            // 4.0.1 如果请求到的数据的长度，小于 pagesize ，说明没数据了
+            if(goods.length < this.pagesize){
+                this.hasMore = false;
+            }
+            
+        })
+    },
+  },
+  // 页面触底事件
+  onReachBottom(){
+    // 页面触底的时候页调用获取数据的函数
+    this.getData();
   },
 }
 </script>
@@ -90,25 +146,22 @@ export default {
             margin-right: 20rpx;
         }
 
-        .goods-title{
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-            -webkit-line-clamp: 2;
-            overflow: hidden;
-        }
-
         .goods-right{
             flex:1;
             display: flex;
             flex-direction:column;
             justify-content: space-between;
-        }
 
-        .price{
-            color:red;
-            font-size: 14px;
-            span{
-                font-size: 22px;
+            .goods-title{
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                overflow: hidden;
+            }
+
+            .price{
+                color:red;
+                font-size: 14px;
             }
         }
     }
