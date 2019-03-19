723ce89d5478e505b8bded33f2b9978aedf4caae
Su <gzqd201802@163.com>
2019-03-19 16:12:04 +0800

    搜索页面结构布局

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,6 +1,33 @@
 <template>
   <view>
-    搜索页  {{keyword}}
+    <!-- 1.0 搜索分区 -->
+    <view class="search-wrapper">
+      <view class="search-input">
+        <icon type="search" size="32rpx"></icon>
+        <input type="text" :value="keyword" placeholder="请输入你想要的商品">
+      </view>
+      <button class="cancel" size="mini">取消</button>
+    </view>
+    <!-- 2.0.1 历史分区 -->
+    <view class="history-title">
+      <text>历史搜索</text>
+      <icon type="clear" size="30rpx"></icon>
+    </view>
+    <!-- 2.0.2 历史列表 -->
+    <view class="history-list">
+      <block v-for="(item,index) in history" :key="index">
+        <view class="history-list-item">小米</view>
+      </block>
+    </view>
+    <!-- 3.0 搜索提示 -->
+    <view class="search-tips" v-show="false">
+      <block v-for="(item,index) in [11,1,1,11,1,111,1,111,1,111,1,111,1,111,1,1]" :key="index">
+        <view class="search-tips-item">
+          提示1
+        </view>
+      </block>
+    </view>
+
   </view>
 </template>
 
@@ -8,7 +35,8 @@
 export default {
   data () {
     return{
-      keyword:''
+      keyword:'',
+      history:[1,2,2,2,1,2,2,2,2,2,1,2,2]
     }
   },
   onLoad(query){
@@ -17,7 +45,70 @@ export default {
 }
 </script>
 
-<style>
-/* 最终编译成 WXSS */
+<style lang="scss">
+// 搜索分区
+.search-wrapper{
+    background: #eee;
+    padding:20rpx;
+    display: flex;
+    .search-input{
+        background: #fff;
+        display: flex;
+        align-items: center;
+        height: 60rpx;
+        flex:1;
 
+        icon{
+            margin:0 10px;
+        }
+
+        input{
+            flex:1;
+            font-size: 14px;
+            padding-right:20rpx;
+        }
+    }
+
+    .cancel{
+        width:160rpx;
+        height:60rpx;
+        padding:0;
+        line-height: 58rpx;
+        margin-left:20rpx;
+    }
+}
+// 历史分区
+.history-title{
+    padding:20rpx;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.history-list{
+    padding:10rpx;
+    &-item{
+        display: inline-block;
+        padding:0 20rpx;
+        line-height: 2;
+        background:#eee;
+        margin:10rpx;
+    }
+}
+// 搜索提示
+.search-tips{
+  position: fixed;
+  left:0;
+  right:0;
+  top:98rpx;
+  bottom:0;
+  background-color: #eee;
+    &-item{
+      border-bottom:1rpx solid #ccc;
+      padding: 20rpx;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+}
 </style>
