8954efae3c8b6de2e0e793d3811924bb377239ab
Su <gzqd201802@163.com>
2019-03-22 18:20:04 +0800

    Revert "Revert "创建订单，获取订单支付信息，调用微信支付，更新支付状态""
    This reverts commit 6cef3b6425b1acb923b66eea46b4772b4c1c54cb.

--- src/api/index.js
+++ src/api/index.js
@@ -18,4 +18,13 @@ export const getDetail = obj => request.get("goods/detail", obj);
 // 用户登录换取 token
 export const userLogin = obj => request.post("users/wxlogin", obj);
 
+// =====================以下接口全部都需要授权后才能访问 ===========
+
+// 创建订单
+export const createOrder = obj => request.auth("my/orders/create", obj);
+// 支付订单
+export const payOrder = obj => request.auth("my/orders/req_unifiedorder", obj);
+// 订单支付状态更新
+export const payOrderUpdata = obj => request.auth("my/orders/chkOrder", obj);
+
 
